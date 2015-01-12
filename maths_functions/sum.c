2af4da46ee1e2e73d7140dac53f6c8a3cec55de7
Emily Elizabeth Willett <P13206095@GH582-018.lec-admin.dmu.ac.uk>
2015-01-12 13:38:40 +0000

    average

--- maths_functions/sum.c
+++ maths_functions/sum.c
@@ -2,8 +2,25 @@
 
 int main()
 {
-	int x = 1;
-	int x = 2;
-	printf ("%i + %i = %i\n", x, y, x+y);
+	int a;
+	printf ("Enter value for a");
+	scanf ("%c", &a);
+	int b;
+	printf ("Enter value for b");
+	scanf ("%c", &b);
+	int c;
+	printf ("Enter value for c");
+	scanf ("%c", &c);
+	int d;
+	printf ("Enter value for d");
+	scanf ("%c", &d);
+	int e;
+	printf ("Enter value for e");
+	scanf ("%c", &e);
+	
+	printf ("%i + %i = %i\n", a, b, c, d, e, a+b+c+d+e);
+	printf ("%i - %i = %i\n", a, b, c, d, e, a-b-c-d-e);
+	printf ("%i * %i = %i\n", a, b, c, d, e, a*b*c*d*e);
+	printf ("%i / %i = %i\n", a, b, c, d, e, a/b/c/d/e);
 	return 0;
 }
