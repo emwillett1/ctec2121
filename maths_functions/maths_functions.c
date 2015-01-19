ccaf208a97f38c6e473ab8d059d6e81bd41eb23e
Emily Elizabeth Willett <P13206095@GH582-003.lec-admin.dmu.ac.uk>
2015-01-19 13:37:54 +0000

    file maths_functions.c to be committed

---
+++ maths_functions/maths_functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  printf ("Enter value for a");
+  scanf ("%c", &a);
+  int b;
+  printf ("Enter value for b");
+  scanf ("%c", &b);
+
+  if (a % b !=0) {
+     printf("The remainder is: %i", a%b);
+}
